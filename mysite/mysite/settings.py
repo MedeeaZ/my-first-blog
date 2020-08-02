0857f62295c17a18e6aa6b16b1b2511a6dcf3b5a
MedeeaZ <medeea.zamciuc@yahoo.com>
2020-08-02 11:59:28 +0100

    I modified MEDIA_ROOT and STATIC_ROOT

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join('mysite', 'static')
 
-MEDIA_ROOT = os.path.join('blog/static', 'media')
+MEDIA_ROOT = os.path.join('mysite', 'media')
 MEDIA_URL = '/media/'
