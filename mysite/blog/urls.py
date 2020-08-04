8df13fc5990a56dda9f7748806f0a03c2ac0587d
MedeeaZ <medeea.zamciuc@yahoo.com>
2020-08-04 20:09:36 +0100

    I added a contact page and a new font

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
     path('post/new/', views.post_new, name = 'post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('contact/', views.contact, name = 'contact'),
+    
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
