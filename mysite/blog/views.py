c6ea2946721517a89324f491aee2ec5df5a4efc6
MedeeaZ <medeea.zamciuc@yahoo.com>
2020-08-05 23:51:45 +0100

    I added login and logout plus a navbar

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
@@ -11,19 +12,20 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit = False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk = post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form' : form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk = pk)
     if request.method == "POST":
@@ -31,7 +33,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk = post.pk)
     else:
@@ -40,3 +41,19 @@ def post_edit(request, pk):
 
 def contact(request):
     return render(request, 'blog/contact.html')
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    post.publish()
+    return redirect('post_detail', pk = pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
