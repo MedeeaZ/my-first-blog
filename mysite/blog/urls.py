5c9bc800e73b46848a8b31b7c8d9ddbbf164724a
MedeeaZ <medeea.zamciuc@yahoo.com>
2020-08-09 20:12:32 +0100

    I added comments

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -17,5 +17,8 @@ urlpatterns = [
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('cs/', views.post_cs, name='post_cs'),
     path('art/', views.post_art, name='post_art'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
